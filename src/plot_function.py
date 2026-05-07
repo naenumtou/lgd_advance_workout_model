38fc5a78f76345671bc4430f48edebdcde4ba75c
LengYi <66057952+naenumtou@users.noreply.github.com>
2026-05-07 18:26:28 +0700

    add plot_recov_type()

--- src/plot_function.py
+++ src/plot_function.py
@@ -44,3 +44,47 @@ def plot_res_type(
     plt.title("Resolution type distribution")
 
     return plt.show()
+
+# Plot recovery by resolution type
+def plot_recov_type(
+    data: pd.DataFrame
+) -> None:
+    
+    """
+    Plot recovery by resolution type.
+
+    Description:
+        Plot recovery by resolution type by month since default.
+
+    Args:
+        data (pd.DataFrame): DataFrame of default cashflow. 1 row per 1 month per 1 default account.
+
+    Returns:
+        Figure: Showing figure from matplotlib.
+
+    Notes:
+        - N/A.
+    """
+
+    plot_df = (
+        data.groupby(
+            ["month_since_default", "resolution_type"],
+            as_index = False
+        ).agg(amount = ("amount", "mean"))
+    )
+
+    plt.figure(figsize = (10, 6))
+    for res_type, g in plot_df.groupby("resolution_type"):
+        plt.plot(
+            g["month_since_default"],
+            g["amount"],
+            label = f"resolution_type: {res_type}"
+        )
+    plt.gca().set_yticklabels([f'{i:,.0f}' for i in plt.gca().get_yticks()])
+    plt.xlabel("Month since default")
+    plt.ylabel("Amount")
+    plt.title("Recovery cashflow by resolution type")
+    plt.legend()
+    plt.tight_layout()
+
+    return plt.show()
