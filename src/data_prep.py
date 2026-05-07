d3ea86d20c790f4b5a84a40ebd0681306aff8286
LengYi <66057952+naenumtou@users.noreply.github.com>
2026-05-07 18:34:16 +0700

    add build_monthly_panel()

--- src/data_prep.py
+++ src/data_prep.py
@@ -115,3 +115,122 @@ def prepare_default_accounts(
     print(f"[INFO]: Number of defualt: {df.shape[0]}")
 
     return df
+
+# Building monthly panel
+def build_monthly_panel(
+    df_accounts: pd.DataFrame,
+    df_cashflows: pd.DataFrame,
+    acc_id_col: str,
+    date_col: str,
+    default_date: str,
+    resolution_col: str,
+    resolution_date: str,
+    resolution_type: str,
+    last_period: pd.Timestamp
+) -> pd.DataFrame:
+    
+    """
+    Building monthly cashflow panel.
+
+    Description:
+        To create monthly cashflow of recovery post default event.
+        The cashflow data is not consecutively months. By running this
+        function, the monthly panel is created for each default account.
+        
+    Args:
+        df_raw (pd.DataFrame)       : Input transaction data.
+        df_cashflow (pd.DataFrame)  : Input cashflow data.
+        acc_id_col (str)            : Primary key.
+        date_col (str)              : Date column in data.
+        default_date (str)          : Default date column in data.
+        resolution_col (str)        : Account status for identify resolution types.
+        resolution_date (str)       : Resolution date column in data.
+        resolution_date (str)       : Resolution type column in data.
+        last_period (pd.Timestamp)  : The latest date in data for modeling.
+
+    Returns:
+        pd.DataFrame: DataFrame of default cashflow. 1 row per 1 month per 1 default account.
+
+    Notes:
+        - For at the date of default event, the cashflow is forced to zero.
+    """
+
+    print("=== Processing ===\n[INFO]: Panel monthly cashflow for default population")
+
+    # Create all possible cashflow ranges
+    months = pd.date_range(
+            start = df_cashflows[date_col].min(),
+            end = df_cashflows[date_col].max() + pd.offsets.MonthEnd(0),
+            freq = "ME",
+        )
+    months = pd.DataFrame(months, columns = [date_col])
+    
+    # Prepare account for mapping
+    account_map = df_cashflows[[acc_id_col]].drop_duplicates()
+    panel = account_map.merge(months, how = "cross")
+    
+    # Map fully cashflow
+    cashflow = (
+        panel
+        .merge(
+            df_cashflows[[acc_id_col, date_col, "amount"]],
+            how = "left",
+            on = [acc_id_col, date_col]
+        )
+        .sort_values(by = [acc_id_col, date_col])
+    )
+
+    # Map default cashflow
+    default_cashflow = pd.merge(
+        cashflow,
+        df_accounts[[acc_id_col, default_date, resolution_date, resolution_type, resolution_col]],
+        how = "left",
+        left_on = [acc_id_col],
+        right_on = [acc_id_col]
+    )
+    
+    # Select only valid cashflow
+    default_cashflow = default_cashflow[
+        (
+            (default_cashflow[date_col] >= default_cashflow[default_date]) &
+            (default_cashflow[date_col] <= default_cashflow[resolution_date])
+        )
+        |
+        (
+            (default_cashflow[resolution_type] == 100) &
+            (default_cashflow[date_col] >= default_cashflow[default_date]) &
+            (default_cashflow[date_col] <= last_period)
+        )
+    ]
+
+    default_cashflow[[resolution_type, resolution_col]] = default_cashflow[[resolution_type, resolution_col]].astype(int)
+
+    # Month since default (After default)
+    default_cashflow["month_since_default"] = (
+        (default_cashflow[date_col].dt.year - default_cashflow[default_date].dt.year) * 12 +
+        (default_cashflow[date_col].dt.month - default_cashflow[default_date].dt.month)
+    )
+    
+    # Clean recovery amount that first month of default should not have recovery
+    default_cashflow["amount"] = np.where(
+        default_cashflow["month_since_default"] == 0,
+        0,
+        default_cashflow["amount"]
+    )
+
+    # Resolution month flag only for resolved cases
+    default_cashflow["is_resolution_month"] = np.where(
+        (default_cashflow[resolution_col] == 1) &
+        (default_cashflow[date_col] == default_cashflow[resolution_date]),
+        1, 0
+    )
+
+    default_cashflow = default_cashflow[
+        [
+            date_col, acc_id_col, "month_since_default",
+            "amount", resolution_type, "is_resolution_month",
+            resolution_col
+        ]
+    ]
+
+    return default_cashflow.reset_index(drop = True)
